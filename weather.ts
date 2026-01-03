bcfa96d161e98392d687620b6cf1eb72c6f98265
boticelli <kandelakitina@gmail.com>
2026-01-03 14:12:04 +0300

    Add destructuring of args

--- weather.ts
+++ weather.ts
@@ -1,8 +1,8 @@
 import { getArgs } from "./helpers/args.ts";
 
 const initCLI = () => {
-  const args = getArgs(Deno.args);
-  console.log(args);
+  const { s: city, h: help, t: token } = getArgs(Deno.args);
+  console.log(city, help, token);
 };
 
 initCLI();
