1328b15a3c948c6e00ab667412a40b03ca5be91a
boticelli <kandelakitina@gmail.com>
2026-01-06 00:14:55 +0300

    Add saveToken

--- weather.ts
+++ weather.ts
@@ -2,10 +2,28 @@ import { getArgs } from "./helpers/args.ts";
 import { printError, printHelp, printSuccess } from "./services/log.service.ts";
 import { saveKeyValue } from "./services/storage.service.ts";
 
+const saveToken = async (token: string) => {
+  if (!token.length) {
+    printError("Token cannot be blank");
+    return;
+  }
+  try {
+    await saveKeyValue("token", token);
+    printSuccess("Token saved");
+  } catch (error) {
+    if (error instanceof Error) {
+      printError(error.message);
+    } else {
+      printError(String(error));
+    }
+    return;
+  }
+};
+
 const initCLI = () => {
   const { s: city, h: help, t: token } = getArgs(Deno.args);
   if (help) printHelp();
-  if (token) saveKeyValue("token", token);
+  if (token !== undefined) saveToken(token);
 };
 
 initCLI();
